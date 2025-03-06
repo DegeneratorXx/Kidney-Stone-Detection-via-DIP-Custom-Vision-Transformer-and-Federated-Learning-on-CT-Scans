4bbcd639db5b8cb5c5cd164e58c3b94b8673e564
Your GitHub Username <bt22eci027@iiitn.ac.in>
2025-03-06 10:31:31 +0530

    tensor-fedml

--- Federated-Model/client.py
+++ Federated-Model/client.py
@@ -1,82 +1,55 @@
 import flwr as fl
-import torch
-import torch.nn as nn
-import torch.optim as optim
-from torch.utils.data import DataLoader
-from torchvision import datasets, transforms
+import tensorflow as tf
+from tensorflow.keras.optimizers import Adam
+from tensorflow.keras.losses import SparseCategoricalCrossentropy
+from tensorflow.keras.metrics import SparseCategoricalAccuracy
 from model import Net
+import numpy as np
 
 class MnistClient(fl.client.NumPyClient):
-    def __init__(self, model, trainloader, testloader):
+    def __init__(self, model, train_data, test_data):
         self.model = model
-        self.device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
-        self.model.to(self.device)  # move model to the detected device
-        self.trainloader = trainloader
-        self.testloader = testloader
+        self.model.compile(
+            optimizer=Adam(learning_rate=0.01),
+            loss=SparseCategoricalCrossentropy(),
+            metrics=[SparseCategoricalAccuracy()]
+        )
+        self.train_data = train_data
+        self.test_data = test_data
 
     def get_parameters(self, config):
-        return [val.cpu().numpy() for val in self.model.state_dict().values()]
+        return [val.numpy() for val in self.model.get_weights()]
 
     def set_parameters(self, parameters):
-        params_dict = zip(self.model.state_dict().keys(), parameters)
-        state_dict = {k: torch.tensor(v) for k, v in params_dict}
-        self.model.load_state_dict(state_dict)
+        self.model.set_weights(parameters)
 
     def fit(self, parameters, config):
         self.set_parameters(parameters)
-        self.model.train()
-        criterion = nn.NLLLoss()
-        optimizer = optim.Adam(self.model.parameters(), lr=0.01)
-
-        for epoch in range(1):
-            for batch_idx, (data, target) in enumerate(self.trainloader):
-                data, target = data.to(self.device), target.to(self.device)
-                optimizer.zero_grad()
-                output = self.model(data)
-                loss = criterion(output, target)
-                loss.backward()
-                optimizer.step()
-
-        return self.get_parameters(config={}), len(self.trainloader), {}
+        self.model.fit(self.train_data, epochs=1, verbose=1)
+        return self.get_parameters({}), len(self.train_data), {}
 
     def evaluate(self, parameters, config):
         self.set_parameters(parameters)
-        self.model.eval()
-        criterion = nn.NLLLoss()
-        correct, total, loss = 0, 0, 0.0
+        loss, accuracy = self.model.evaluate(self.test_data, verbose=0)
+        return loss, accuracy, {}
 
-        with torch.no_grad():
-            for data, target in self.testloader:
-                data, target = data.to(self.device), target.to(self.device)
-                outputs = self.model(data)
-                loss += criterion(outputs, target).item()
-                _, predicted = torch.max(outputs.data, 1)
-                total += target.size(0)
-                correct += (predicted == target).sum().item()
-
-        return loss / len(self.testloader), correct / total, {}
-
-def load_data(is_train):
-    transform = transforms.Compose([
-        transforms.ToTensor(),
-        transforms.Normalize((0.1307,), (0.3081,))
-    ])
-    dataset = datasets.MNIST(
-        "./data", train=is_train, download=True, transform=transform
-    )
-    return DataLoader(dataset, batch_size=32, shuffle=True)
+def load_data():
+    (x_train, y_train), (x_test, y_test) = tf.keras.datasets.mnist.load_data()
+    x_train, x_test = x_train / 255.0, x_test / 255.0
+    x_train = x_train[..., np.newaxis]
+    x_test = x_test[..., np.newaxis]
+    
+    train_data = tf.data.Dataset.from_tensor_slices((x_train, y_train)).batch(32).shuffle(10000)
+    test_data = tf.data.Dataset.from_tensor_slices((x_test, y_test)).batch(32)
+    
+    return train_data, test_data
 
 def main():
     model = Net()
-    trainloader = load_data(is_train=True)
-    testloader = load_data(is_train=False)
-
-    client = MnistClient(model, trainloader, testloader)
-    # Convert the NumPyClient to a Client instance using .to_client()
-    fl.client.start_client(
-        server_address="localhost:8080",
-        client=client.to_client()
-    )
+    train_data, test_data = load_data()
+    client = MnistClient(model, train_data, test_data)
+    fl.client.start_client(server_address="localhost:8080", client=client)
 
 if __name__ == "__main__":
     main()
+
